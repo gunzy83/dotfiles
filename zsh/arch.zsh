ccb39cc112da8a25628898443c1ec7683aed292f
Ross Williams <gunzy83au@gmail.com>
2016-02-28 12:47:02 +1000

    Added os detection and conditional alias on arch aliases.

--- zsh/arch.zsh
+++ zsh/arch.zsh
@@ -1,10 +1,12 @@
 #!/bin/zsh
 
-# Use packer from packer.io instead of the packer AUR helper
-alias packer="packer-io"
+if [ $DIST = "arch" ] ; then
+    # Use packer from packer.io instead of the packer AUR helper
+    alias packer="packer-io"
 
-# Find what package provides a file (remember to pkgfile --update)
-alias whatprovides="pkgfile "
+    # Find what package provides a file (remember to pkgfile --update)
+    alias whatprovides="pkgfile "
 
-# Find what files a package provides
-alias provideswhat="pacman -Ql "
+    # Find what files a package provides
+    alias provideswhat="pacman -Ql "
+fi
