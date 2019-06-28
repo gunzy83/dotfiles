dfcc745934a6ad8a71d463a6cc91ae80c8e1f3f6
Ross Williams <gunzy83au@gmail.com>
2019-06-28 11:21:43 +1000

    Update pyenv and python version.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
 echo "Inititalising submodules..."
-git submodule init
-git submodule update
+#git submodule init
+#git submodule update
 
 echo "Linking pyenv and pyenv-virtualenv..."
 ln -sfn $(pwd)/pyenv ~/.pyenv
@@ -11,11 +11,11 @@ ln -sfn $(pwd)/pyenv-virtualenv ~/.pyenv/plugins/pyenv-virtualenv
 echo "Preparing pyenv in the current shell"
 source zsh/pyenv.zsh
 
-echo "Installing python version 2.7.11 with pyenv"
-pyenv install 2.7.11 -s
+echo "Installing python version 2.7.16 with pyenv"
+pyenv install 2.7.16 -s
 
 echo "Creating virtualenv for dotfiles"
-pyenv virtualenv 2.7.11 dotfiles
+pyenv virtualenv 2.7.16 dotfiles
 
 echo "Activating the virtualenv and adding .python-version file"
 pyenv activate dotfiles
