50b6410751a3aded0a1a12352b7f39fe50ad3dfc
Ross Williams <gunzy83au@gmail.com>
2019-11-20 12:34:17 +1000

    Work Updates
    * Update newbranch git alias
    * Update Sublime settings again
    * Add functions to switch virtualenv in pyenv from python 2 to 3 with a single command

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -93,6 +93,35 @@ passwordgen() {
     tr -cd '[:alnum:]' < /dev/urandom | fold -w${@:-20} | head -n1
 }
 
+_create-virtualenv() {
+    venv_name=$1/envs/$2
+    pyenv install $1 --skip-existing
+    pyenv virtualenv $1 $2 && pyenv activate $venv_name && pip install -r requirements.txt && pyenv deactivate $venv_name && echo $venv_name > .python-version
+}
+
+create-virtualenv-py27() {
+    _create-virtualenv 2.7.16 $1
+}
+
+create-virtualenv-py368() {
+    _create-virtualenv 3.6.8 $1
+}
+
+create-virtualenv-py371() {
+    _create-virtualenv 3.7.1 $1
+}
+
 create-virtualenv() {
-    pyenv virtualenv 2.7.11 $1 && pyenv activate $1 && pip install -r requirements.txt && pyenv deactivate $1 && echo $1 > .python-version
+    create-virtualenv-py368 $1
+}
+
+remove-virtualenv() {
+    version=$(pyenv version-name)
+    rm .python-version
+    pyenv virtualenv-delete $version
+}
+
+recreate-virtualenv() {
+    remove-virtualenv
+    create-virtualenv $1
 }
